425b5c2e09c2a3b754e737280c8a9798effda216
Vick Greenfields <akintoluvic@users.noreply.github.com>
2020-06-13 20:23:40 +0100

    create text element fn

--- Index.js
+++ Index.js
@@ -14,3 +14,14 @@ function createElement(type, props, ...children) {
     },
   }
 }
+
+// wrap everything that isn’t an object inside its own element and create a special type for them: TEXT_ELEMENT.
+function createTextElement(text) {
+    return {
+      type: "TEXT_ELEMENT",
+      props: {
+        nodeValue: text,
+        children: [],
+      },
+    }
+  }
