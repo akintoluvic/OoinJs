a01bae917b6336f788a1603380f4f2c5a3d5181f
Vick Greenfields <akintoluvic@users.noreply.github.com>
2020-06-13 20:51:11 +0100

    cleanup

--- Index.js
+++ Index.js
@@ -1,28 +1,19 @@
-/** @jsx Didact.createElement */
-const element = (
-    <div id="foo">
-      <a>bar</a>
-      <b />
-    </div>
-  )
-
 function createElement(type, props, ...children) {
-  return {
-    type,
-    props: {
-      ...props,
-      children: children.map(child =>
-        typeof child === "object"
-          ? child
-          : createTextElement(child)
-      ),
-      children,
-    },
+    return {
+      type,
+      props: {
+        ...props,
+        children: children.map(child =>
+          typeof child === "object"
+            ? child
+            : createTextElement(child)
+        ),
+      },
+    }
   }
-}
-
-// wrap everything that isn’t an object inside its own element and create a special type for them: TEXT_ELEMENT.
-function createTextElement(text) {
+  ​
+  // wrap everything that isn’t an object inside its own element and create a special type for them: TEXT_ELEMENT.
+  function createTextElement(text) {
     return {
       type: "TEXT_ELEMENT",
       props: {
@@ -31,17 +22,39 @@ function createTextElement(text) {
       },
     }
   }
-
-const Ragnar = {
-    createElement
-}
-
-const element = Ragnar.createElement(
-"div",
-{ id: "foo" },
-Ragnar.createElement("a", null, "bar"),
-Ragnar.createElement("b")
-)
-
-const container = document.getElementById("root")
-Ragnar.render(element, container)
+  ​
+  function render(element, container) {
+    const dom =
+      element.type == "TEXT_ELEMENT"
+        ? document.createTextNode("")
+        : document.createElement(element.type)
+  ​
+    const isProperty = key => key !== "children"
+    Object.keys(element.props)
+      .filter(isProperty)
+      .forEach(name => {
+        dom[name] = element.props[name]
+      })
+  ​
+    element.props.children.forEach(child =>
+      render(child, dom)
+    )
+  ​
+    container.appendChild(dom)
+  }
+  ​
+  const Ragnar = {
+    createElement,
+    render,
+  }
+  ​
+  /** @jsx Ragnar.createElement */
+  const element = (
+    <div id="foo">
+      <a>bar</a>
+      <b />
+    </div>
+  )
+  const container = document.getElementById("root")
+  Ragnar.render(element, container)
+  
