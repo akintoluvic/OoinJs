a2f5bc3d125e757d17a224a86706c88fcc1943d5
Vick Greenfields <akintoluvic@users.noreply.github.com>
2020-06-26 19:29:14 +0100

    init

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+var path = require('path')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {test: /\.(js)$/, use: 'babel-loader'},
+            {test: /\.css$/, use: ['style-loader', 'css-loader']},
+        ]
+    },
+    mode: 'development',
+
+}
